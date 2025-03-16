c572acf1d19cf03a7c4aed6beaffef13c1c949e6
nikiiigoyal <nikiiigoyal@gmail.com>
2025-03-16 22:31:03 +0530

    Modified about me section

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -125,7 +125,7 @@ const About = () => {
     sr.reveal(revealContainer.current, srConfig());
   }, []);
 
-  const skills = ['JavaScript (ES6+)', 'TypeScript', 'React', 'Eleventy', 'Node.js', 'WordPress'];
+  const skills = ['JavaScript (ES6+)', 'TypeScript', 'React', 'Html', 'CSS', 'Tailwind Css'];
 
   return (
     <StyledAboutSection id="about" ref={revealContainer}>
