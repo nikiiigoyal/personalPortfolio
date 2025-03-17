7b9eb5f316838bdcf5164f97e7453e15119812aa
nikiiigoyal <nikiiigoyal@gmail.com>
2025-03-17 15:00:49 +0530

    Modified hero section

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -7,7 +7,7 @@ export { default as IconFork } from './fork';
 export { default as Icon } from './icon';
 export { default as IconGitHub } from './github';
 export { default as IconHex } from './hex';
-export { default as IconInstagram } from './instagram';
+export { default as IconHackerRank } from './HackerRank';
 export { default as IconLinkedin } from './linkedin';
 export { default as IconLoader } from './loader';
 export { default as IconLogo } from './logo';
