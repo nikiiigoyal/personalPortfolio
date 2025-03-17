fa42729bcb173ab74fce8c097d602603e40258ba
nikiiigoyal <nikiiigoyal@gmail.com>
2025-03-17 14:58:06 +0530

    Add new componnet hackerrank

---
+++ src/components/icons/HackerRank.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const IconHackerRank = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    role="img"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className="feather feather-hackerrank"
+  >
+    <title>HackerRank</title>
+    {/* <!-- Hexagon background (usually green in colored version) --> */}
+    <path d="M12 2L4 7v10l8 5 8-5V7l-8-5z"></path>
+    {/* <!-- H letter --> */}
+    <path d="M8.5 8v8M15.5 8v8M8.5 12h7"></path>
+  </svg>
+);
+
+export default IconHackerRank;
